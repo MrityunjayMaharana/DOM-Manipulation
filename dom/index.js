30a4a5b0a99bedae0aaaf8f7bb5e50f69d9aaca9
Mrityunjay Maharana <115680134+MrityunjayMaharana@users.noreply.github.com>
2022-12-17 12:19:01 +0530

    Add files via upload

---
+++ dom/index.js
@@ -0,0 +1,11 @@
+
+let k = prompt("enter your name :")
+let l = prompt("enter your age :")
+document.getElementById("mytext").innerHTML="MY";
+document.getElementById("name").innerHTML="name";
+document.getElementById("is").innerHTML="is";
+document.getElementById("myname").innerHTML=k;
+document.getElementById("my").innerHTML="my"
+document.getElementById("age").innerHTML="age";
+document.getElementById("is").innerHTML="is";
+document.getElementById("myage").innerHTML=l;
